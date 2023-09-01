258dfac53a8ca3281f452b89006cc441cb2693d2
Héctor Segura <hd.segura@uniandes.edu.co>
2023-08-31 20:40:58 -0500

    web_scraping_hector.R

---
+++ scripts/tests/web_scraping_hector.R
@@ -0,0 +1,22 @@
+library(tidyverse); library(rvest)
+
+rm(list = ls())
+setwd("/Users/hectorsegura/Documentos/Big Data & ML/Taller 1 ")
+getwd()
+
+#Probando con la primera tabla por separado
+HTML1 <- read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/page1.html")
+#Se observa que la tabla está dentro de "w3-include-html" de la página 'pages/geih_page_1.html' 
+
+tablas_url<-paste0("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_", 1:10, ".html")
+
+df<-data.frame()
+for (url in tablas_url){
+  print(url)
+  temp<-read_html(url) %>% html_table() 
+  temp<-as.data.frame(temp[[1]])
+  df<-rbind(df, temp)
+}
+
+write_csv(df, file = "datos.csv") 
+
