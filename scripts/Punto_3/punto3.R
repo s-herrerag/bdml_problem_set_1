d3a30ae28c0ce372e4473810f1603346d2bfa46b
Santiago Herrera Garcia <santiago.cgarcia99@gmail.com>
2023-09-17 13:17:41 -0500

    Unificacion punto 3

---
+++ scripts/Punto_3/punto3.R
@@ -0,0 +1,82 @@
+## Primera regresión: Edad
+
+
+# Carga de paquetes y datos requeridos. -------------------------------------------
+
+library(pacman)
+p_load(this.path, rvest, tidyverse, stargazer, ggplot2, boot)
+file_dir <- this.path::here()
+setwd(file_dir)
+
+
+# Transformación de variables que exige el modelo. ------------------------
+
+df_clean <- read.csv("../Punto_2/BaseFinal.csv") %>%
+  mutate(logInc = log(y_ingLab_m_ha), age2 = age^2) ## Arreglar variable salario
+
+df_clean <- df_clean %>%
+  mutate(age2 = age^2)
+
+
+# Regresión ---------------------------------------------------------------
+
+salario.edad.lm <- lm(logInc ~ age + age2, data = df_clean)
+stargazer(salario.edad.lm, type = "latex",
+          out="outputs/reg_salario_edad.tex")
+
+
+# Gráfico de dispersión de datos con la curva que describe el modelo --------
+
+age_logInc_plot <- ggplot(df_clean, aes(y = logInc, x = age)) +
+  geom_point() +
+  stat_smooth(
+    formula = "y ~ x + I(x^2)", method = "lm", se = TRUE,
+    linewidth = 1
+  ) +
+  theme_bw() +
+  labs(
+    x = "Edad",
+    y = "Logaritmo del Ingreso"
+  )
+ggsave("outputs/reg_age_inc.png", age_logInc_plot, dpi=300)
+
+# Punto 3: Bootstrap  -----------------------------------------------------
+
+#### Funcion bootstrap
+age_boot <- function (df, index){
+  
+  df_reg <-df[index,]
+  
+  #Regresion para calcular los coeficientes
+  reg <- lm(formula = logInc ~ age + age2,
+            data = df_reg)
+  b1 <-  reg$coefficients[2]
+  b2 <- reg$coefficients[3]
+  
+  #Estadistico de interes: Maximo analitico de edad
+  max_income_age <- -(b1/(2*b2))
+  max_income_age
+}
+
+#### Estimacion bootstrap
+set.seed(415)
+bootstrap_results <- boot(df_clean, age_boot, R = 1000)
+
+#Output (Tabla con valores de bootstrap)
+output_boot <- broom::tidy(bootstrap_results)
+output_boot$mean_boot <- mean(bootstrap_results$t)
+
+output_boot <- output_boot %>%
+  mutate(across(c(statistic, bias, std.error, mean_boot), ~round(., 2))) %>%
+  mutate(term="Edad de máximos ingresos") %>%
+  select(c(term, statistic, mean_boot, bias, std.error))
+
+colnames(output_boot) <- c("", "Estimación muestra original", "Media muestras bootstrap (R=1000)", "Sesgo", "Error estándar")
+
+stargazer(output_boot, summary = FALSE, digits = 2, type = "latex", rownames = F,
+          out = "outputs/results_boot.tex")
+
+
+
+
+
--- scripts/Punto_3/regresion_y_grafico.R
+++ scripts/Punto_3/regresion_y_grafico.R
@@ -11,7 +11,7 @@ setwd(file_dir)
 
 # Transformación de variables que exige el modelo. ------------------------
 
-df_clean <- read.csv("../../stores/geih_scraped.csv") %>%
+df_clean <- read.csv("../../scripts/Punto_2/BaseFinal.csv") %>%
   mutate(logInc = log(y_ingLab_m_ha), age2 = age^2) ## Arreglar variable salario
 
 df_clean <- df_clean %>%
@@ -38,3 +38,29 @@ ggplot(df_clean, aes(y = logInc, x = age)) +
     y = "Logaritmo del Ingreso",
     title = "Perfiles de Edad - Ingreso estimados, implicados por el modelo"
   )
+
+# Punto 3: Bootstrap  -----------------------------------------------------
+require(pacman)
+p_load(tidyverse, boot)
+
+#### Cargar Datos
+df_clean <- read.csv("../stores/geih.csv")
+
+#### Funcion bootstrap
+age_boot <- function (df, index){
+  
+  df_reg <-df[index,]
+  
+  #Regresion para calcular los coeficientes
+  reg <- lm(formula = income ~ age + age2,
+            data = df_reg)
+  b1 <-  reg$coefficients[1]
+  b2 <- reg$coefficients[2]
+  
+  #Estadistico de interes: Maximo analitico de edad
+  max_income_age <- -(b1/2*b2)
+  max_income_age
+}
+
+#### Estimacion bootstrap
+bootstrap_results <- boot(df_clean, age_boot, R = 1000)
