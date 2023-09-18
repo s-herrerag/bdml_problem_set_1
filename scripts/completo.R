ab59a4dc325e4ae27fd01c93ec37dd3d04509611
Catalinad18 <catalinadc18@gmail.com>
2023-09-17 23:03:11 -0500

    actualización de orden

--- scripts/completo.R
+++ scripts/completo.R
@@ -1,11 +1,11 @@
 ##############################################################
 #       Big Data y Machine Learning                          #
-#       Taller 1 - Punto 4                                   #
+#       Taller 1 - Código completo                           #
 ##############################################################
 
 #-------------------------------------------
 # Load packages
-pkg <- list("dplyr", "readr", "tidyverse", "rio", "stargazer", "boot")
+pkg <- list("dplyr", "readr", "tidyverse", "rio", "stargazer", "boot", "rvest", "xml2")
 lapply(pkg, require, character.only = T)
 rm(pkg)
 
@@ -13,9 +13,48 @@ rm(pkg)
 rm(list = ls())
 #-------------------------------------------
 
-# Load data ---------------------------------
-base <- read_csv("stores/geih_scraped.csv")
-set.seed(123)
+############################################
+#Web Scrapping
+############################################
+
+# Main web
+url_base <- "https://ignaciomsarmiento.github.io/GEIH2018_sample/"
+main <- read_html(url_base)
+
+# Links to the data
+links_data <- (main %>% html_nodes("a") %>% xml_attr("href"))[seq(from = 7, to = 16)]
+urls <- paste0(url_base, links_data)
+
+# Extract data of each link
+data_frame_list <- list()
+
+for (url in urls) {
+  page <- read_html(url)
+  link_node <- page %>%
+    html_nodes("div.col-md-9") %>%
+    html_nodes("div") %>%
+    xml_attr("w3-include-html")
+  url_page <- paste0(url_base, link_node)
+  table <- read_html(url_page) %>%
+    html_table() %>%
+    as.data.frame()
+  data_frame_list[[url]] <- table
+}
+
+# Merge data
+final_data <- bind_rows(data_frame_list)
+write.csv(final_data, "../bdml_problem_set_1/stores/geih.csv")
+
+
+
+
+
+
+
+
+############################################
+#Punto 4
+############################################
 
 # Item a ------------------------------------
 base <- base[!is.na(base$y_ingLab_m_ha), ]
@@ -44,3 +83,8 @@ fn <- function(data, index) {
 }
 
 boot(base, fn, R = 1000)
+
+
+
+
+
